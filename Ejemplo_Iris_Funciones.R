2b9573d2cc99473adb152a706efffb6b42e69bf7
Ludwing-MJ <mjludwing27@gmail.com>
2025-06-11 18:17:51 -0600

    subida inicial

---
+++ Ejemplo_Iris_Funciones.R
@@ -0,0 +1,11 @@
+# UNIVERSIDAD DE SAN CARLOS DE GUATEMALA
+# FACULTAD DE AGRONOMÍA
+# CENTRO DE TELEMÁTICA -CETE-
+# EJEMPLO PARA ESTADISTICA DESCRIPTIVA PARA DATOS AGRUPADOS
+# P. Agr. Ludwing Isaí Marroquín Jiménez
+
+# Enlace Github Ejemplo:
+# https://ludwing-mj.github.io/R-para-el-analisis-estadistico-de-datos-
+
+# Repositorio Github Script: 
+# https://github.com/Ludwing-MJ/MTCDPR_datos_agrupados.git
